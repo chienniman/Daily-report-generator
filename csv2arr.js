909873e623ffd471b9d21b2bcce319841e60b56b
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-08-23 00:41:28 +0800

    v2

---
+++ csv2arr.js
@@ -0,0 +1,42 @@
+$.fn.csv2arr = function (callback) {
+	if (typeof FileReader == "undefined") {
+		alert("Browser is too old,please use Chrome or Firefox");
+		return false;
+	}
+	if (!$(this)[0].files[0]) {
+		alert("Please select a file");
+		return false;
+	}
+	var fReader = new FileReader();
+	fReader.readAsDataURL($(this)[0].files[0]);
+	$fileDOM = $(this);
+	fReader.onload = function (evt) {
+		var data = evt.target.result;
+		var encoding = checkEncoding(data);
+
+		Papa.parse($($fileDOM)[0].files[0], {
+			encoding: encoding,
+			complete: function (results) {
+				var res = results.data;
+				if (res[res.length - 1] == "") {
+					res.pop();
+				}
+				callback && callback(res);
+			},
+		});
+	};
+	fReader.onerror = function (evt) {
+		alert("The file has changed,please select again");
+	};
+
+	function checkEncoding(base64Str) {
+		var str = atob(base64Str.split(";base64,")[1]);
+		var encoding = jschardet.detect(str);
+		encoding = encoding.encoding;
+
+		if (encoding == "windows-1252") {
+			encoding = "ANSI";
+		}
+		return encoding;
+	}
+};
