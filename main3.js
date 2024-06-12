06ad5b4cd244b09f0beeed516d1e0751d5a76ff8
Boris <cscs851210max@gmail.com>
2024-06-12 17:27:31 +0800

    [Chore]新增當日銷售代號

--- main3.js
+++ main3.js
@@ -17,6 +17,18 @@ let visitedAreas = [];
 // 11 PRDTMLQY 銷貨量
 // 12 PRDTMIQY 庫存量
 
+// 當日銷售代號
+// 0 DATYM 資料年月
+// 1 PTVCOD 供應商代號
+// 2 PTVCNA 供應商簡稱
+// 3 PTDPNO 門市代號
+// 4 PTDPNA 門市名稱
+// 5 PRDTCODE 貨號
+// 6 CVCIT 條碼
+// 7 PUPRNA 品名 / 規格
+// 8 PRDTMLQY 銷貨量
+
+
 // 績效總表代號
 // C 區，D 店數，E 業績占比，F 業績目標，G 業績達成，H 達成 % 數
 
