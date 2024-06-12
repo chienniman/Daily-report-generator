b873c53f9bbc0cfb5376b018fe108916876bdc8f
Boris <cscs851210max@gmail.com>
2024-06-12 21:25:42 +0800

    [Doc]刪除舊文件，新增代號

--- main3.js
+++ main3.js
@@ -8,35 +8,6 @@ import { appendHeaderRows, clearTableAndInput } from "./helpers/table.js";
 
 let visitedAreas = [];
 
-// 進銷存代號
-// 0 DATYM 資料年月
-// 1 PTVCOD 供應商代號
-// 2 PTVCNA 供應商簡稱
-// 3 PTDPNO 門市代號
-// 4 PTDPNA 門市名稱
-// 5 PRDTCODE 貨號
-// 6 CVCIT 條碼
-// 7 PUPRNA 品名 / 規格
-// 8 PRDTMSQY 上存量
-// 9 PRDTMPQY 進貨量
-// 10 PRDTMBQY 退貨量
-// 11 PRDTMLQY 銷貨量
-// 12 PRDTMIQY 庫存量
-
-// 當日銷售代號
-// 0 DATYM 資料年月
-// 1 PTVCOD 供應商代號
-// 2 PTVCNA 供應商簡稱
-// 3 PTDPNO 門市代號
-// 4 PTDPNA 門市名稱
-// 5 PRDTCODE 貨號
-// 6 CVCIT 條碼
-// 7 PUPRNA 品名 / 規格
-// 8 PRDTMLQY 銷貨量
-
-// 績效總表代號
-// C 區，D 店數，E 業績占比，F 業績目標，G 業績達成，H 達成 % 數
-
 function reset() {
   sessionStorage.clear();
 
