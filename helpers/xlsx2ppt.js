d7df583b77dec3ecbda1bf77a7b62af335a046f1
Boris <cscs851210max@gmail.com>
2024-06-16 23:42:41 +0800

    [Test]生成單頁PPT

--- helpers/xlsx2ppt.js
+++ helpers/xlsx2ppt.js
@@ -1,6 +1,85 @@
+function createPPT() {
+  let pptx = new PptxGenJS();
+
+  let slide = pptx.addSlide();
+
+  let rows = [
+    [
+      { text: "", options: { bold: true, fill: "99cdff" } },
+      { text: "陳列品項", options: { bold: true, fill: "99cdff" } },
+      { text: "陳列位", options: { bold: true, fill: "99cdff" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+    [
+      { text: "大甲民生", options: { fill: "99cdff" } },
+      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
+      { text: "陳列位", options: { fill: "b5c7dd" } },
+    ],
+  ];
+
+  let tabOpts = {
+    align: "center",
+    valign: "middle",
+    fontSize: 18,
+    colW: [2.2, 5.2, 1.7],
+    rowH: 0.35,
+    border: { pt: "1", color: "FFFFFF" },
+  };
+
+  slide.addTable(rows, tabOpts);
+
+  pptx.writeFile({ fileName: "TableDemo.pptx" });
+}
+
 document
   .getElementById("xlsxFileInput")
   .addEventListener("change", function (e) {
+    createPPT();
+
     var files = e.target.files;
     if (!files.length) return;
     var file = files[0];
@@ -8,7 +87,6 @@ document
     var reader = new FileReader();
     reader.onloadend = async function (event) {
       var arrayBuffer = reader.result;
-
       var workbook = new ExcelJS.Workbook();
       await workbook.xlsx.load(arrayBuffer);
 
@@ -18,12 +96,17 @@ document
         worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
           let rowData = [];
           row.eachCell(function (cell, colNumber) {
-            if (cell.value) {
+            // 店號、店名、陳列品項、陳列位
+            if (cell.value && [3, 4, 7, 8].includes(colNumber)) {
               rowData.push(cell.value);
             }
+
+            // if (cell.value) {
+            //     rowData.push(cell.value);
+            //   }
           });
           if (rowData.length > 0) {
-            // console.log(`Row ${rowNumber}: ${rowData.join(", ")}`);
+            console.log(`Row ${rowNumber}: ${rowData.join(", ")}`);
           }
         });
 
@@ -45,18 +128,18 @@ document
           const col = image.range.tl.nativeCol;
           const cellAddress = `${getColumnLetter(col)}${row}`;
 
-          console.log(`${img.name}.${img.extension}`, cellAddress);
+          //   console.log(`${img.name}.${img.extension}`, cellAddress);
 
           const base64data = img.buffer.toString("base64");
 
-        //   var imgElement = document.createElement("img");
+          //   var imgElement = document.createElement("img");
 
-        //   imgElement.src = "data:image/png;base64," + base64data;
-        //   imgElement.style.width = "6cm";
-        //   imgElement.style.height = "8cm";
+          //   imgElement.src = "data:image/png;base64," + base64data;
+          //   imgElement.style.width = "6cm";
+          //   imgElement.style.height = "8cm";
 
-        //   var container = document.getElementById("imageContainer");
-        //   container.appendChild(imgElement);
+          //   var container = document.getElementById("imageContainer");
+          //   container.appendChild(imgElement);
         });
       });
     };
