cf4c7e0f7e4dfa8a9671d0e80873384e8fc70256
Boris <cscs851210max@gmail.com>
2024-06-17 12:45:01 +0800

    [Test]createPPT、createPhotos

--- helpers/xlsx2ppt.js
+++ helpers/xlsx2ppt.js
@@ -1,67 +1,16 @@
-function createPPT() {
-  let pptx = new PptxGenJS();
+function getColumnLetter(colIndex) {
+  let letter = "";
+  while (colIndex >= 0) {
+    letter = String.fromCharCode((colIndex % 26) + 65) + letter;
+    colIndex = Math.floor(colIndex / 26) - 1;
+  }
+  return letter;
+}
 
-  let slide = pptx.addSlide();
-
-  let rows = [
-    [
-      { text: "", options: { bold: true, fill: "99cdff" } },
-      { text: "陳列品項", options: { bold: true, fill: "99cdff" } },
-      { text: "陳列位", options: { bold: true, fill: "99cdff" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-    [
-      { text: "大甲民生", options: { fill: "99cdff" } },
-      { text: "檸檬兩封", options: { fill: "b5c7dd" } },
-      { text: "陳列位", options: { fill: "b5c7dd" } },
-    ],
-  ];
-
-  let tabOpts = {
+function createPPT(data) {
+  const pptx = new PptxGenJS();
+  const slide = pptx.addSlide();
+  const tabOpts = {
     align: "center",
     valign: "middle",
     fontSize: 18,
@@ -70,16 +19,33 @@ function createPPT() {
     border: { pt: "1", color: "FFFFFF" },
   };
 
-  slide.addTable(rows, tabOpts);
-
+  slide.addTable(data, tabOpts);
   pptx.writeFile({ fileName: "TableDemo.pptx" });
+}
+
+function createPhotos(workbook,worksheet) {
+  worksheet.getImages().forEach(async function (image, index) {
+    const img = workbook.model.media.find((m) => m.index === image.imageId);
+    const row = image.range.tl.nativeRow + 1;
+    const col = image.range.tl.nativeCol;
+    const cellAddress = `${getColumnLetter(col)}${row}`;
+    //   console.log(`${img.name}.${img.extension}`, cellAddress);
+    const base64data = img.buffer.toString("base64");
+
+    var imgElement = document.createElement("img");
+
+    imgElement.src = "data:image/png;base64," + base64data;
+    imgElement.style.width = "6cm";
+    imgElement.style.height = "8cm";
+
+    var container = document.getElementById("imageContainer");
+    container.appendChild(imgElement);
+  });
 }
 
 document
   .getElementById("xlsxFileInput")
   .addEventListener("change", function (e) {
-    createPPT();
-
     var files = e.target.files;
     if (!files.length) return;
     var file = files[0];
@@ -90,57 +56,30 @@ document
       var workbook = new ExcelJS.Workbook();
       await workbook.xlsx.load(arrayBuffer);
 
+      const displayTable = [];
+
       workbook.eachSheet(function (worksheet, sheetId) {
         console.log(`Sheet ${sheetId}: ${worksheet.name}`);
-
-        worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
+        worksheet.eachRow({ includeEmpty: true }, function (row, rowId) {
           let rowData = [];
           row.eachCell(function (cell, colNumber) {
-            // 店號、店名、陳列品項、陳列位
-            if (cell.value && [3, 4, 7, 8].includes(colNumber)) {
+            // 店名、陳列品項、陳列位
+            if (cell.value && [4, 7, 8].includes(colNumber)) {
               rowData.push(cell.value);
             }
-
-            // if (cell.value) {
-            //     rowData.push(cell.value);
-            //   }
           });
-          if (rowData.length > 0) {
-            console.log(`Row ${rowNumber}: ${rowData.join(", ")}`);
+          if (rowData.length === 3) {
+            const data = [
+              { text: rowData[0], options: { fill: "99cdff" } },
+              { text: rowData[1], options: { fill: "b5c7dd" } },
+              { text: rowData[2], options: { fill: "b5c7dd" } },
+            ];
+            displayTable.push(data);
           }
         });
 
-        function getColumnLetter(colIndex) {
-          let letter = "";
-          while (colIndex >= 0) {
-            letter = String.fromCharCode((colIndex % 26) + 65) + letter;
-            colIndex = Math.floor(colIndex / 26) - 1;
-          }
-          return letter;
-        }
-
-        worksheet.getImages().forEach(async function (image, index) {
-          const img = workbook.model.media.find(
-            (m) => m.index === image.imageId
-          );
-
-          const row = image.range.tl.nativeRow + 1;
-          const col = image.range.tl.nativeCol;
-          const cellAddress = `${getColumnLetter(col)}${row}`;
-
-          //   console.log(`${img.name}.${img.extension}`, cellAddress);
-
-          const base64data = img.buffer.toString("base64");
-
-          //   var imgElement = document.createElement("img");
-
-          //   imgElement.src = "data:image/png;base64," + base64data;
-          //   imgElement.style.width = "6cm";
-          //   imgElement.style.height = "8cm";
-
-          //   var container = document.getElementById("imageContainer");
-          //   container.appendChild(imgElement);
-        });
+        createPPT(displayTable);
+        createPhotos(workbook,worksheet);
       });
     };
     reader.readAsArrayBuffer(file);
