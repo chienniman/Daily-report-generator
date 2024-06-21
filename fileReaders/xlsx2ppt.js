8f00e6c04c8f4035df5cba515a715a6f20ab6451
Boris <cscs851210max@gmail.com>
2024-06-21 18:26:29 +0800

    [Feat]新增pace.js

--- fileReaders/xlsx2ppt.js
+++ fileReaders/xlsx2ppt.js
@@ -59,11 +59,14 @@ $(document).ready(function () {
     const file = e.target.files[0];
     if (!file) return;
 
+    Pace.restart();
+
     const reader = new FileReader();
     reader.onloadend = async function () {
       const workbook = new ExcelJS.Workbook();
       await workbook.xlsx.load(reader.result);
       createPPT(workbook);
+      Pace.stop();
     };
     reader.readAsArrayBuffer(file);
   });
