e16ed2728616453faec73c78c6ce984d6e7b0003
Boris <cscs851210max@gmail.com>
2024-06-24 22:48:12 +0800

    [Update]generateReport

--- components/actionButtons/generateReport.js
+++ components/actionButtons/generateReport.js
@@ -1,4 +1,3 @@
-import { processDailyKpi } from "/utils/fileProcessing.js";
 import { createReport } from "/utils/report.js";
 import { validateInputs } from "/utils/validation.js";
 import { button } from "../shared/buttons/button.js";
@@ -7,7 +6,6 @@ $(document).ready(function () {
   $(".top-row").append(button({ id: "generateReport", text: "生成" }));
 
   $("#generateReport").on("click", async function () {
-    processDailyKpi(dailyKpi);
     if (!validateInputs()) return;
     Pace.restart();
     await createReport();
