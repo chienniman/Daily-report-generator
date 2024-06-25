354e1ac9ca4681986eb5462d742ca50c8b6d1a72
Boris <cscs851210max@gmail.com>
2024-06-25 16:04:23 +0800

    [Update]button.js

--- components/shared/buttons/button.js
+++ components/shared/buttons/button.js
@@ -3,6 +3,7 @@ import { mergeStyleString } from "/helpers/mergeStyleString.js";
 function button({ id, text, styles }) {
   const fixedStyles = {
     "max-width": "80px",
+    cursor: "pointer",
   };
 
   return `<button id="${id}" class="button" style="${mergeStyleString(
