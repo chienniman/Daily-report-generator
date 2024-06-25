5885c9b8248ca25345ac8387d5dca2cea490805c
Boris <cscs851210max@gmail.com>
2024-06-25 10:41:12 +0800

    [Create]reset

---
+++ components/actionButtons/reset.js
@@ -0,0 +1,27 @@
+import { button } from "../shared/buttons/button.js";
+
+$(document).ready(function () {
+  $(".top-row").append(
+    button({
+      id: "reset",
+      text: "清空",
+      styles: { display: 'none' }
+    })
+  );
+
+  $("#reset").on("click", async function () {
+    function reset() {
+      sessionStorage.clear();
+
+      $("#fileNameDisplay, #resultTable").empty();
+      $(
+        "input[name=monthStocks], input[name=todaySells], input[name=dailyKpi]"
+      ).val("");
+      $(
+        "#monthStocksFileNameDisplay, #todaySellsFileNameDisplay, #dailyKpiFileNameDisplay"
+      ).text("");
+    }
+
+    reset();
+  });
+});
