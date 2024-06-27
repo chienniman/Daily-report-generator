21ac0fa2b34a3d94bcab9ae8600ffb921a545598
Boris <cscs851210max@gmail.com>
2024-06-27 13:33:35 +0800

    [Create]新增app.js

---
+++ components/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  function createBaseElement() {
+    $("body").append(`
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+            <div class="content">
+                <h1>全聯報表生成器</h1>
+            </div>
+            <div id="uploadedFiles"></div>
+            <div class="top-row"></div>
+            <table id="resultTable" border="1"></table>
+        `);
+  }
+
+  createBaseElement();
+});
---
+++ dist/components/app.js
@@ -0,0 +1,19 @@
+(() => {
+  // components/app.js
+  $(document).ready(function() {
+    function createBaseElement() {
+      $("body").append(`
+            <div class="bg"></div>
+            <div class="bg bg2"></div>
+            <div class="bg bg3"></div>
+            <div class="content">
+                <h1>\u5168\u806F\u5831\u8868\u751F\u6210\u5668</h1>
+            </div>
+            <div id="uploadedFiles"></div>
+            <div class="top-row"></div>
+            <table id="resultTable" border="1"></table>
+        `);
+    }
+    createBaseElement();
+  });
+})();
