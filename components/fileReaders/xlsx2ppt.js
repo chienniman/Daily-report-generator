84200aa3215912b75f0397e9f2eea6c8ddb4a116
Boris <cscs851210max@gmail.com>
2024-06-27 16:39:23 +0800

    [Update]樣式移到index.scss

--- components/fileReaders/xlsx2ppt.js
+++ components/fileReaders/xlsx2ppt.js
@@ -2,46 +2,6 @@ import { createPPT } from "../../helpers/createPPT.js";
 import { fileInput } from "../shared/fileInput.js";
 
 $(document).ready(function () {
-  function createStyle() {
-    const styles = `
-      #pptTableContainer>table>tbody{
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-      }
-      #pptTableContainer>table>tbody>tr{
-          display: flex;
-          justify-content: center;
-          width: 100%;
-      }
-      #pptTableContainer>table>tbody>tr>td{
-          padding: 0;
-          border: 1px solid;
-          width: 4cm;
-      }
-      #pptTableContainer{
-          position: absolute;
-          z-index: 10;
-          background-color: whitesmoke;
-      }
-      .store-td{
-          background-color: #99cdff;
-      }
-      .empty-td,.description-td{
-          width: 1cm!important;
-          writing-mode: vertical-lr;
-          background-color: #99cdff;
-      }
-      .photo-table{
-          border-collapse: collapse;
-      }
-  `;
-
-    $("<style>").text(styles).appendTo("head");
-  }
-
-  createStyle();
-
   function createBaseElement() {
     $(".top-row").append(
       fileInput({
