6bba651c17e6741bd169ba289cc7ab3696b051d4
Boris <cscs851210max@gmail.com>
2024-06-29 22:18:55 +0800

    [Feat]報表生成reset

--- utils/report.js
+++ utils/report.js
@@ -1,14 +1,19 @@
 import { productMap } from "../dataSets/pxMarts.js";
 import { appendHeaderRows, appendTableRows } from "../helpers/appendRows.js";
 import { processCSV } from "./fileProcessing.js";
+import { resetFileInputs, resetTable } from "./resetUtilities.js";
 
 async function createReport() {
   try {
+    resetTable();
+
     const monthStocksData = await processCSV("monthStocks");
     const todaySellsData = await processCSV("todaySells");
 
     appendHeaderRows(productMap);
     appendTableRows($("#resultTable"), monthStocksData, todaySellsData);
+
+    resetFileInputs();
   } catch (error) {
     console.error("報表生成錯誤", error);
   }
