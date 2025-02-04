230dbef254560183094af5a4344e0a6d15b06e67
Boris <97031067+chienniman@users.noreply.github.com>
2025-02-04 22:50:14 +0800

    [Update] 隱藏水位

--- components/shared/table/row/headerRow.js
+++ components/shared/table/row/headerRow.js
@@ -1,8 +1,18 @@
+// function headerRow(productMap) {
+//   let headerRow = "<tr class='gray'><th>處</th><th>區</th><th>店名</th>";
+
+//   Array.from(productMap.values()).forEach((product) => {
+//     headerRow += `<th colspan=3">${product}</th>`;
+//   });
+
+//   return headerRow + "</tr>";
+// }
+
 function headerRow(productMap) {
   let headerRow = "<tr class='gray'><th>處</th><th>區</th><th>店名</th>";
 
   Array.from(productMap.values()).forEach((product) => {
-    headerRow += `<th colspan=3">${product}</th>`;
+    headerRow += `<th colspan=2">${product}</th>`;
   });
 
   return headerRow + "</tr>";
--- components/shared/table/row/subHeaderRow.js
+++ components/shared/table/row/subHeaderRow.js
@@ -1,27 +1,48 @@
+// function subHeaderRow(productMap) {
+//   let subHeaderRow = "<tr class='white'><th></th><th></th><th></th>";
+
+//   Array.from(productMap.values()).forEach(() => {
+//     subHeaderRow += `
+//               <th id="threshold">
+//                   <div class='darkred-text'>
+//                       水位
+//                   </div>
+//               </th>
+//               <th id="stock">
+//                   <div class='darkred-text'>
+//                       庫存
+//                   </div>
+//               </th>
+//               <th id="sell">
+//                   <div class='darkred-text'>
+//                       日銷
+//                   </div>
+//               </th>
+//           `;
+//   });
+
+//   return subHeaderRow + "</tr>";
+// }
+
 function subHeaderRow(productMap) {
-  let subHeaderRow = "<tr class='white'><th></th><th></th><th></th>";
-
-  Array.from(productMap.values()).forEach(() => {
-    subHeaderRow += `
-              <th id="threshold">
-                  <div class='darkred-text'>
-                      水位
-                  </div>
-              </th>
-              <th id="stock">
-                  <div class='darkred-text'>
-                      庫存
-                  </div>
-              </th>
-              <th id="sell">
-                  <div class='darkred-text'>
-                      日銷
-                  </div>
-              </th>
-          `;
-  });
-
-  return subHeaderRow + "</tr>";
-}
+    let subHeaderRow = "<tr class='white'><th></th><th></th><th></th>";
+  
+    Array.from(productMap.values()).forEach(() => {
+      subHeaderRow += `
+                <th id="stock">
+                    <div class='darkred-text'>
+                        庫存
+                    </div>
+                </th>
+                <th id="sell">
+                    <div class='darkred-text'>
+                        日銷
+                    </div>
+                </th>
+            `;
+    });
+  
+    return subHeaderRow + "</tr>";
+  }
 
 export { subHeaderRow };
